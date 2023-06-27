c6337bf153395f8c6f6c3285672e59a7aa71d9e7
mulla-sajid <sajidmulla394@gmail.com>
2023-06-27 16:03:40 +0530

    New li element added without the same class name.

--- dom.js
+++ dom.js
@@ -36,12 +36,27 @@
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
 console.log(items[1]);
-// items[1].textContent='Hello 2';
-// items[1].style.fontWeight='bold';
-// items[1].style.backgroundColor='yellow';
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
 
 // items.style.backgroundColor='#f4f4f4';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
-items[2].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+// items[2].style.backgroundColor='green';
+
+
+// GET ELEMENT BY TAG NAME//
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
+
+// li.style.backgroundColor='#f4f4f4';
+// for(var i=0;i<li.length;i++){
+//     li[i].style.fontWeight='bold';
+// }
+li[2].style.backgroundColor='green';
