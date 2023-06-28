967a7681ce5eecb394257f5ab613f3e04a9c23f2
mulla-sajid <sajidmulla394@gmail.com>
2023-06-28 11:18:30 +0530

    1) The background color of 2nd item has been changed to green 2) The third item is now invisible.

--- dom.js
+++ dom.js
@@ -33,12 +33,12 @@
 
 
 //GET ELEMENT BY CLASS NAME//
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2';
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='yellow';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='yellow';
 
 // items.style.backgroundColor='#f4f4f4';
 // for(var i=0;i<items.length;i++){
@@ -48,10 +48,10 @@ items[1].style.backgroundColor='yellow';
 
 
 // GET ELEMENT BY TAG NAME//
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='Hello 2';
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2';
 // li[1].style.fontWeight='bold';
 // li[1].style.backgroundColor='yellow';
 
@@ -59,4 +59,26 @@ li[1].textContent='Hello 2';
 // for(var i=0;i<li.length;i++){
 //     li[i].style.fontWeight='bold';
 // }
-li[2].style.backgroundColor='green';
+// li[2].style.backgroundColor='green';
+
+// QUERYSELECTOR //
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = 'Send';
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color='blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor='green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none';
