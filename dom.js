a45965e65af7a75babbc4507013c66392438ace5
mulla-sajid <sajidmulla394@gmail.com>
2023-06-28 13:11:04 +0530

    1) Hello world is added before Item Lister. 2) Hello world is added before item 1.

--- dom.js
+++ dom.js
@@ -1,94 +1,69 @@
-//Examine the documnet object//
-// console.dir(document);
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.title);
-// // document.title=123;
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-// console.log(document.all);
-// console.log(document.all[9]);
-// // document.all[9].textContent='Hello';
-// console.log(document.forms[0]);
-// console.log(document.links);
-// console.log(document.images);
+//TRAVERSING THE DOM//
+var itemList = document.querySelector('#items');
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode);
+// console.log(itemList.parentNode.parentNode.parentNode);
 
-//GET ELEMENT BY ID//
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor='#f4f4f4';
+// console.log(itemList.parentElement.parentElement);
+// console.log(itemList.parentElement.parentElement.parentElement);
 
-// console.log(document.getElementById('header-title'));
-// var headerTitle = document.getElementById('header-title');
-// var header = document.getElementById('main-header');
-// var addTitle = document.getElementsByClassName('title');
-// console.log(headerTitle.textContent);
-// headerTitle.textContent = 'Hello';
-// headerTitle.innerText = 'Goodbye';
-// console.log(headerTitle.innerHTML);
-// headerTitle.innerHTML = '<h3>Hello</h3>';
-// headerTitle.style.borderBottom = 'solid 3px #000';
-// header.style.borderBottom = 'solid 3px #000';
-// console.log(addTitle);
-// addTitle[0].style.fontWeight='bold';
-// addTitle[0].style.color='green';
+// // childNode
+// // console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+// itemList.children[1].style.backgroundColor='yellow';
 
+// // firstChild
+// console.log(itemList.firstChild);
+// // firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent='Hello1';
+// // lastChild
+// console.log(itemList.lastChild);
+// // lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent='Hello4';
 
-//GET ELEMENT BY CLASS NAME//
-// var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent='Hello 2';
-// items[1].style.fontWeight='bold';
-// items[1].style.backgroundColor='yellow';
+// // nextSibling
+// console.log(itemList.nextSibling);
+// // nextElementSibling
+// console.log(itemList.nextElementSibling);
+// // previousSibling
+// console.log(itemList.previousSibling);
+// // previousElementSibling
+// console.log(itemList.previousElementSibling);
 
-// items.style.backgroundColor='#f4f4f4';
-// for(var i=0;i<items.length;i++){
-//     items[i].style.fontWeight='bold';
-// }
-// items[2].style.backgroundColor='green';
+// createElement
+// Create a div
+var newDiv = document.createElement('div');
+// Add class
+newDiv.className='hello';
+// Add id
+newDiv.id='hello1';
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+// Create text node
+var newDivText = document.createTextNode('Hello World');
+// Add text to div
+newDiv.appendChild(newDivText);
 
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+// console.log(newDiv);    
+container.insertBefore(newDiv, h1);
 
-// GET ELEMENT BY TAG NAME//
-// var li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-// li[1].textContent='Hello 2';
-// li[1].style.fontWeight='bold';
-// li[1].style.backgroundColor='yellow';
+var newItem = document.createElement('li');
+newItem.className='list-group-item';
+console.log(newItem);
+var newLiText = document.createTextNode('HEllo world');
+newItem.appendChild(newLiText);
 
-// li.style.backgroundColor='#f4f4f4';
-// for(var i=0;i<li.length;i++){
-//     li[i].style.fontWeight='bold';
-// }
-// li[2].style.backgroundColor='green';
+var ul = document.querySelector('div .list-group');
+var listLi = document.querySelector('div li');
+ul.insertBefore(newItem, listLi);
 
-// QUERYSELECTOR //
-// var header = document.querySelector('#main-header');
-// header.style.borderBottom = 'solid 4px #ccc';
-
-// var input = document.querySelector('input');
-// input.value = 'Hello World';
-
-// var submit = document.querySelector('input[type="submit"]');
-// submit.value = 'Send';
-
-// var item = document.querySelector('.list-group-item');
-// item.style.color='red';
-
-// var lastItem = document.querySelector('.list-group-item:last-child');
-// lastItem.style.color='blue';
-
-// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-// secondItem.style.backgroundColor='green';
-
-// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-// thirdItem.style.display='none';
-
-// QUERY SELECTOR ALL//
-var titles = document.querySelectorAll('.list-group-item');
-// console.log(titles);
-titles[1].style.color='green';
-
-var li = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor='green';
-}
