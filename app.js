2344755a03188368dba344e4ec9aefe9901c1186
mulla-sajid <sajidmulla394@gmail.com>
2023-06-29 16:44:18 +0530

    User details stored inside local storage.

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Working on git'); 
+// var form = document.querySelector('#userFrom');
+var subBtn = document.querySelector('#submitBtn');
+subBtn.addEventListener('click', storeUser);
+
+function storeUser(e){
+    e.preventDefault();
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+
+    var uDetails ={name:name,email:email,phone:phone};
+    var userDetailsJson = JSON.stringify(uDetails);
+    localStorage.setItem('userDetails',userDetailsJson);
+
+    alert('Date stored');
+}
+ 
