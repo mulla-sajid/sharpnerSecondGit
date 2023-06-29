16622d388c56d109a2772b3516f70b389a131b72
mulla-sajid <sajidmulla394@gmail.com>
2023-06-29 12:55:36 +0530

    1) Delete button functionality added. 2) Edit button is added next to delete button.

--- dom.js
+++ dom.js
@@ -1,69 +1,51 @@
-//TRAVERSING THE DOM//
-var itemList = document.querySelector('#items');
-// parentNode
-// console.log(itemList.parentNode);
-// itemList.parentNode.style.backgroundColor = '#f4f4f4';
-// console.log(itemList.parentNode.parentNode);
-// console.log(itemList.parentNode.parentNode.parentNode);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
-// parentElement
-// console.log(itemList.parentElement);
-// itemList.parentElement.style.backgroundColor='#f4f4f4';
-// console.log(itemList.parentElement.parentElement);
-// console.log(itemList.parentElement.parentElement.parentElement);
+// form submit event
+form.addEventListener('submit', addItem);
+// delete event
+itemList.addEventListener('click', removeItem);
 
-// // childNode
-// // console.log(itemList.childNodes);
-// console.log(itemList.children);
-// console.log(itemList.children[1]);
-// itemList.children[1].style.backgroundColor='yellow';
+// add item
+function addItem(e){
+e.preventDefault();
 
-// // firstChild
-// console.log(itemList.firstChild);
-// // firstElementChild
-// console.log(itemList.firstElementChild);
-// itemList.firstElementChild.textContent='Hello1';
-// // lastChild
-// console.log(itemList.lastChild);
-// // lastElementChild
-// console.log(itemList.lastElementChild);
-// itemList.lastElementChild.textContent='Hello4';
+// get input value
+var newItem = document.getElementById('item').value;
 
-// // nextSibling
-// console.log(itemList.nextSibling);
-// // nextElementSibling
-// console.log(itemList.nextElementSibling);
-// // previousSibling
-// console.log(itemList.previousSibling);
-// // previousElementSibling
-// console.log(itemList.previousElementSibling);
+// create new li element
+var li = document.createElement('li');
+li.className='list-group-item';
+li.appendChild(document.createTextNode(newItem));
 
-// createElement
-// Create a div
-var newDiv = document.createElement('div');
-// Add class
-newDiv.className='hello';
-// Add id
-newDiv.id='hello1';
-// Add attr
-newDiv.setAttribute('title', 'Hello Div');
-// Create text node
-var newDivText = document.createTextNode('Hello World');
-// Add text to div
-newDiv.appendChild(newDivText);
+// create delete button
+var deleteBtn = document.createElement('button');
+deleteBtn.className='btn btn-danger btn-sm float-end delete';
+deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+}
 
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-// console.log(newDiv);    
-container.insertBefore(newDiv, h1);
 
-var newItem = document.createElement('li');
-newItem.className='list-group-item';
-console.log(newItem);
-var newLiText = document.createTextNode('HEllo world');
-newItem.appendChild(newLiText);
+// remove item
+function removeItem(e){
+if(e.target.classList.contains('delete')){
+    if(confirm('Are you sure')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
+}
 
-var ul = document.querySelector('div .list-group');
-var listLi = document.querySelector('div li');
-ul.insertBefore(newItem, listLi);
+
+// add edit button
+var li = document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++){
+    var editBtn = document.createElement('button');
+editBtn.className='btn btn-primary btn-sm float-end';
+editBtn.appendChild(document.createTextNode('Edit'));   
+    var liItem = li[i];
+    liItem.insertBefore(editBtn,liItem.lastChild);
+}
+
 
